a470fc8fa6f5c0d5f838f44f83c758a9e8b0588b
Rich Alesi <walesi@andrew.cmu.edu>
2011-12-08 15:51:32 -0500

    Working version

--- opera/background.js
+++ opera/background.js
@@ -20,12 +20,38 @@ function loadInjectedJS(event, path) {
 
 }
 
+function loadInjectedCSS(event, path) {
+    // Function uses path to get JS file
+    var req = new XMLHttpRequest();
+    req.open('GET', path, false);
+    req.send();
+    // Error check for reading the JS file.
+    if (!req.responseText) {
+        opera.postError('EXTENSION ERROR: Can\'t read ' + path);
+        return;
+    }
+
+    // Send the contents of the JS file to the injected script.
+    event.source.postMessage({
+        topic: 'LoadedInjectedCSS',
+        data: {
+            css: req.responseText,
+            path: path
+        }
+    });
+
+}
+
 function onMessage(event) {
     var message = event.data;
     // Check the correct message has been received and send the JS file path to loadInjectedJS().
     if (message.topic == 'LoadInjectedJS') {
         var path = message.data;
         loadInjectedJS(event, path);
+    }
+    else if (message.topic == 'LoadInjectedCSS') {
+        var path = message.data;
+        loadInjectedCSS(event, path);
     }
 }
 
--- opera/includes/injected.js
+++ opera/includes/injected.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
     // Specify the path to the stylesheet here:
-    var path = 'http://localhost:3131/' + window.location.hostname.replace('www.','') + '.js';
+    var path = 'http://localhost:3131/' + window.location.hostname.replace('www.','');
 
     // Error check for the JavaScript filename.
     if (!path) {
@@ -12,9 +12,9 @@ window.addEventListener('DOMContentLoaded', function() {
         var message = event.data;
 
         // Check this is the correct message and path from the background script.
-        if (message.topic === 'LoadedInjectedJS' && message.data.path === path) {
+        if (message.topic === 'LoadedInjectedJS' && message.data.path === path + '.js') {
             // Remove the message listener so it doesn't get called again.
-            opera.extension.removeEventListener('message', onJS, false);
+            // opera.extension.removeEventListener('message', onJS, false);
 
             var js = message.data.js;
 
@@ -24,15 +24,38 @@ window.addEventListener('DOMContentLoaded', function() {
             script.appendChild(document.createTextNode(js));
             document.getElementsByTagName('head')[0].appendChild(script);
         }
+    // }
+    // var onCSS = function(event) {
+        // var message = event.data;
+
+        // Check this is the correct message and path from the background script.
+    else if (message.topic === 'LoadedInjectedCSS' && message.data.path === path + '.css') {
+            // Remove the message listener so it doesn't get called again.
+            // opera.extension.removeEventListener('message', onCSS, false);
+
+            var css = message.data.css;
+
+            // Create a <script> element and add it to the <head> element of the current page.
+            // Insert the contents of the stylesheet into the <script> element.
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.appendChild(document.createTextNode(css));
+            document.getElementsByTagName('head')[0].appendChild(style);
+        }
     }
 
     // On receipt of a message from the background script, execute onJS().
     opera.extension.addEventListener('message', onJS, false);
+    // opera.extension.addEventListener('message', onCSS, false);
 
     // Send the JS file path to the background script to get the JS.
     opera.extension.postMessage({
         topic: 'LoadInjectedJS',
-        data: path
+        data: path + '.js'
+    });
+    opera.extension.postMessage({
+        topic: 'LoadInjectedCSS',
+        data: path + '.css'
     });
 
 }, false);
